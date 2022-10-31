d4b3ac15c0b4b765f0f00d3c7d847ef504ddbf93
andermendz <andersonmendz19@gmail.com>
2022-10-31 18:21:47 -0500

    responsive UI improved with toggle

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -54,7 +54,7 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
 
     return (
         <>
-            <SideBar logout={logout} />
+            
             <div className="channel-list__list__wrapper">
                 <CompanyHeader />
                 <ChannelSearch setToggleContainer={setToggleContainer} />
@@ -107,6 +107,7 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
                     )}
                 />
             </div>
+            <SideBar logout={logout} />
         </>
     );
 }
@@ -125,7 +126,7 @@ const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) =>
             </div>
 
             <div className="channel-list__container-responsive"
-                style={{ left: toggleContainer ? "0%" : "-85%", backgroundColor: "#005fff"}}
+                style={{ right: toggleContainer ? "-85%" : "0%", backgroundColor: "#005fff"}}
             >
                 <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
                 </div>
